bc6e274845e5fa7150b24baa1a1850c0b648a6cb
faraz <mdsarfarazidrishi@gmail.com>
2024-10-28 15:15:41 +0530

    addred email validator using regex(regular expression

---
+++ emailvalidator2.py
@@ -0,0 +1,22 @@
+import re
+#example@gmail.com
+"""regex: regular expression 
+# \d - digit
+# \w - word character [a-zA-Z0-9_]
+# \s - whitespace
+# . - any character except newline
+# ^ - start of string
+# $ - end of string
+# * - 0 or more occurrences
+# + - 1 or more occurrences
+# ? - 0 or 1 occurrence
+# {n} - exactly n occurrences
+# {n,} - n or more occurrences
+# {n,m} - between n and m occurrences"""
+
+email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email=input("Enter email address: ")
+if re.search(email_condition, user_email):
+    print("sahi hai email")
+else:
+    print("invalid format")
