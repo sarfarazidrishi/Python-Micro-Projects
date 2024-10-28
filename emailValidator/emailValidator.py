6e492d55a57b7222c9851f77b4912fdeed3e5d72
faraz <mdsarfarazidrishi@gmail.com>
2024-10-28 12:32:53 +0530

    updated the logic

--- emailValidator/emailValidator.py
+++ emailValidator/emailValidator.py
@@ -6,25 +6,25 @@ if len(email)>=6:
             if (email[-4]==".") ^ (email[-3]=="."):
                 for i in email:
                     if i==i.isspace():
-                        k=1
+                        k=1 #just a variable being increased, will use it later to tell that there is space
                     elif i.isalpha():
                         if i==i.upper():
-                            j=1
+                            j=1 #checks if there is upper case ...used for testing and showing result
                     elif i.isdigit():
-                        continue
+                        continue #if there is digit then it's fine move to next step
                     elif i =="_" or i =="." or i=="@":
-                        continue
+                        continue #if there is @, . or _ then it's fine move to next step
                     else:
                         d=1
                 if k==1 or j ==1 or d==1:
-                    print("email me spaxe in dalte hai balak")
+                    print("email me spaxe, capital letters in dalte hai balak")
                 else:
-                    pass
+                    print("valid email")
             else:
-                print("email me ek hi dot hoga domain me")
+                print("email me ek hi dot hoga domain me 4")
         else:
             print("email should have at most @ in it 3")
     else:
         print("email must start with a alphabet 2")
 else:
-    print("wrong email 1")
+    print("how come your email is less than 6 characters 1")
